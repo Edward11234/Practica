5939c60a1a23e6bb1472d4e480583263fc1648a3
Edward11234 <136965013+Edward11234@users.noreply.github.com>
2023-07-07 17:42:05 +0300

    Add files via upload

--- signup.php
+++ signup.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
+
+
+if (isset($_SESSION['success_message'])) {
+    echo '<p class="success-message">' . $_SESSION['success_message'] . '</p>';
+    unset($_SESSION['success_message']);
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (empty($_POST["name"])) {
         addError("Introduceți un nume!");
@@ -26,40 +33,40 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         addError("Parolele nu corespund!");
     }
 
-    if (count($_SESSION['signup_form_errors']) > 0) {
-        redirectBack('signup.php');
-    }
-
-    $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
-    $mysqli = require __DIR__ . "/database.php";
-
-    $sql = "INSERT INTO user (name, email, password_hash)
-            VALUES (?, ?, ?)";
-
-    $stmt = $mysqli->stmt_init();
-
-    if (!$stmt->prepare($sql)) {
-        die("SQL error: " . $mysqli->error);
-    }
-
-    $stmt->bind_param("sss", $_POST["name"], $_POST["email"], $password_hash);
-
-    if ($stmt->execute()) {
-        header("Location: signup-success.html");
-        exit;
+    if (count($_SESSION['signup_form_errors'] ?? []) > 0) {
+        redirectBack();
     } else {
-        if ($mysqli->errno === 1062) {
-            addError("Emailul este folosit!");
-            redirectBack('signup.php');
+        $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+        $mysqli = require __DIR__ . "/database.php";
+
+        $sql = "INSERT INTO user (name, email, password_hash)
+                VALUES (?, ?, ?)";
+
+        $stmt = $mysqli->stmt_init();
+
+        if (!$stmt->prepare($sql)) {
+            die("SQL error: " . $mysqli->error);
+        }
+
+        $stmt->bind_param("sss", $_POST["name"], $_POST["email"], $password_hash);
+
+        if ($stmt->execute()) {
+            $_SESSION['success_message'] = 'Registered Successfully';
+            redirectBack();
         } else {
-            die($mysqli->errno . " " . $mysqli->error);
+            if ($mysqli->errno === 1062) {
+                addError("Emailul este folosit!");
+                redirectBack();
+            } else {
+                die($mysqli->errno . " " . $mysqli->error);
+            }
         }
     }
 }
 
-function redirectBack($url) {
-    header('Location: ' . $url);
+function redirectBack() {
+    header('Location: signup.php');
     exit();
 }
 
@@ -71,32 +78,35 @@ function addError($error) {
 }
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-        <title>Signup</title>
-        <style>
-            .error-message {
-                display: none;
-                color: red;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Signup</title>
+    <style>
+        .error-message {
+            display: none;
+            color: red;
+        }
+        .success-message {
+            color: green;
+        }
+    </style>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var errorMessage = document.querySelector(".error-message");
+            if (errorMessage) {
+                errorMessage.style.display = "block";
+                setTimeout(function() {
+                    errorMessage.style.display = "none";
+                }, 3000);
             }
-        </style>
-        <script>
-            document.addEventListener("DOMContentLoaded", function() {
-                var errorMessage = document.querySelector(".error-message");
-                if (errorMessage) {
-                    errorMessage.style.display = "block";
-                    setTimeout(function() {
-                        errorMessage.style.display = "none";
-                    }, 3000);
-                }
-            });
-        </script>
-    </head>
-    <body>
-    <h1>Signup</h1>
+        });
+    </script>
+</head>
+<body>
+<h1>Signup</h1>
 
 <?php
 if (isset($_SESSION['signup_form_errors']) && count($_SESSION['signup_form_errors']) > 0) {
@@ -107,29 +117,29 @@ if (isset($_SESSION['signup_form_errors']) && count($_SESSION['signup_form_error
 }
 ?>
 
-    <form action="signup.php" method="post" novalidate>
-        <div>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name">
-        </div>
+<form action="signup.php" method="post" novalidate>
+    <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name">
+    </div>
 
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email">
-        </div>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+    </div>
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
-        </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password">
+    </div>
 
-        <div>
-            <label for="password_confirmation">Repeat Password</label>
-            <input type="password" id="password_confirmation" name="password_confirmation">
-        </div>
+    <div>
+        <label for="password_confirmation">Repeat Password</label>
+        <input type="password" id="password_confirmation" name="password_confirmation">
+    </div>
 
-        <button type="submit">Sign up</button>
-    </form>
+    <button type="submit">Sign up</button> <p>Acum te poti loga <a href="login.php">aici</a></p>
+</form>
 
-    </body>
+</body>
 </html>
